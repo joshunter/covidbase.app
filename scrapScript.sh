34c3d5678a03c90611e0b39ba4a549b90267bc2e
Josh Hunter <j6hunter@ucsd.edu>
2020-05-20 14:19:18 -0700

    removed countries tables, made world stats, script formatting data

--- scrapScript.sh
+++ scrapScript.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 rm worldometer.html
-rm worlddata
+rm countryData
+rm worldInfo
+rm worldFinal
 rm cutData
 
 curl https://www.worldometers.info/coronavirus/ >> worldometer.html
 
 input="worldometer.html"
-file="worlddata"
+wFile="worldInfo"
+wFile2="worldFinal"
+file="countryData"
 file2="cutData"
 
 found=0
 done=0
 
 i=0
+worldinfoStart='<td style="display:none;" data-continent=""></td>'
 table='<table id="main_table_countries_today"'
 tableEnd='<td><strong>Total:</strong></td'
 regex='\+?(\w+\s?\w*|\d*,?\d*,?\d*\s*)+</(a|td)>'
 
 while IFS= read -r line
 do
+	if [[ $line =~ $regex && $found -eq 1 && $done -eq 0 ]]; then
+		echo "${BASH_REMATCH[0]//'</td>'/''}" >> $wFile
+	fi
 	if [[ $line =~ $regex && $found -eq 2 && $done -eq 0 ]]; then
 		echo "${BASH_REMATCH[0]//'</td>'/''}" >> $file
 	fi
 	# find <table id="main_table_countries_today"
-	if [[ $line =~ $table ]]; then
+	if [[ $line =~ $worldinfoStart ]]; then
 		found=1
 	fi
 	# find country (case sensitive)
@@ -42,8 +50,29 @@ do
 	echo "${line//'</a>'/''}" >> $file2
 done < "$file"
 
-rm worlddata
+rm countryData
 # cut data is done
+
+# Global information
+i=0
+declare -a dataFields=( "{rank: \"" "name: \"" "total: \"" "newActive: \"" "deaths: \"" "newDeaths: \"" "recovered: \"" "active: \"" "critical: \"" "casesPM: \"" "deathsPM: \"")
+
+while IFS= read -r line
+do
+	if [[ i -eq 9 ]]; then
+		echo "${dataFields[$i]}$line\"}" >> $wFile2
+		break
+	else
+		echo "${dataFields[$i]}$line\"," >> $wFile2
+		let "i=i+1"
+	fi
+
+done < "$wFile"
+
+
+
+# Country Info
+
 i=0
 declare -a dataFields=( "{rank: \"" "name: \"" "total: \"" "newActive: \"" "deaths: \"" "newDeaths: \"" "recovered: \"" "active: \"" "critical: \"" "casesPM: \"" "deathsPM: \"" "tests: \"" "testsPM: \"" "population: \"" "continent: \"")
 
@@ -58,3 +87,6 @@ do
 	fi
 
 done < "$file2"
+
+head -n -2 $file >> $file
+
---
+++ src/components/WorldTable.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="worldTable">
+		<div class="title">Global Data</div>
+		<div class="top row">
+			<div class="rowElement active">Active
+					<div>{{worldInfo.active}}</div>
+				<div class="subNumber">{{worldInfo.newActive}}</div>
+			</div>
+			<div class="rowElement recovered">Recovered
+				<div>{{worldInfo.recovered}}</div>
+			</div>
+			<div class="rowElement critical">Critical
+				<div>{{worldInfo.critical}}</div>
+			</div>
+			<div class="rowElement deaths">Deaths
+				<div>{{worldInfo.deaths}}</div>
+				<div class="subNumber">{{worldInfo.newDeaths}}</div>
+			</div>
+			<div class="rowElement">Total
+				<div>{{worldInfo.total}}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+
+<script>
+export default {
+	name: "WorldTable",
+	props: ["worldInfo"]
+}
+</script>
+
+
+<style>
+
+.worldTable {
+    width:90%;
+    border-radius: 15px;
+    background-color: #1b395d;
+	color: #d8dbe2;
+}
+
+.row {
+	display: flex;
+    font-size: 80%;
+    text-align: center;
+    padding-bottom: 5px;
+    padding-left: 3.5%;
+    padding-right: 4%;
+    padding-top: 5px;
+}
+.title {
+    text-align: center;
+	font-size: 125%;
+    padding-top: 5px;
+}
+.rowElement {
+	display: block;
+    text-align: center;
+	width: 20%;
+}
+.subNumber {
+	font-size: 81%;
+}
+.active {
+	color: #58A4B0;
+}
+.recovered {
+	color:  #a0ed8c;
+}
+.critical {
+	color:  #ffaf3f;
+}
+.deaths {
+	color: #ff6575;
+}
+
+</style>
