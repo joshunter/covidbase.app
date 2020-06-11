de4ee8267b7583b7ed8ece05957427635e02667b
Josh Hunter <j6hunter@ucsd.edu>
2020-06-11 02:56:41 -0700

    added TOP button to scroll to top

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app" class="app">
     <Header/>
-<!--     <transition name="fade">
+    <transition name="fade">
       <router-view></router-view>
-    </transition> -->
-    <Homepage/>
+    </transition>
+    <div @click="toTop()" id="ArrowBox" class="ArrowBox ArrowBox--hidden"><i class="arrow"></i></div>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header.vue';
-import Homepage from './components/Homepage.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Homepage
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll () {
+      const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
+
+      if (currentScrollPosition < 0) {
+        return
+      }
+      if (currentScrollPosition > 138){
+        document.getElementById("ArrowBox").classList.remove("ArrowBox--hidden");
+      }
+      if (currentScrollPosition < 138){
+        document.getElementById("ArrowBox").classList.add("ArrowBox--hidden");
+      }
+    },
+    toTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
   }
 }
 </script>
@@ -35,7 +57,7 @@ export default {
   margin-right: auto;
 }
 
-.app {;
+.app {
   display: block;
   min-height: 100vh;
   padding-bottom: 30px;
@@ -45,13 +67,47 @@ export default {
   background-color:  #1b2e4b;
   color: #d8dbe2;
 }
-
+.arrow {
+  border: solid #d8dbe2;
+  border-width: 0 5px 5px 0;
+  display: inline-block;
+  padding: 5px;
+  transform: rotate(-135deg);
+  -webkit-transform: rotate(-135deg);
+  position: fixed;
+  bottom: 105px;
+  right: 32px
+}
+.ArrowBox {
+  border-radius: 7px;
+  height: 50px;
+  width: 50px;
+  background-color:  #3061a1;
+  color: #d8dbe2;
+  position: fixed;
+  bottom: 90px;
+  right: 15px;
+}
+.ArrowBox:hover {
+  background-color:  #24497a;
+  color: #bcc1cd;
+  box-shadow: 0px 0px 10px 3px #182a43;
+}
+.ArrowBox.ArrowBox--hidden {
+  display: none;
+}
+.ArrowBox.lightMode {
+  box-shadow: 0px 0px 10px 0px #e1e1e1;
+}
+.ArrowBox.lightMode:hover {
+  background-color:  #f8f8f8;
+  box-shadow: 0px 0px 5px 0px #e1e1e1;;
+}
 .lightMode {
   background-color: #FFFFFF;
 }
-
 .fade-enter-active {
-  transition: opacity .75s;
+  transition: opacity 1.25s;
 }
 .fade-leave-active {
   transition: opacity 0.25s;
