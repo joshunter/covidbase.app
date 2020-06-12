64142cf0f883a5fde5a02916815fd94770e6a851
Josh Hunter <j6hunter@ucsd.edu>
2020-06-11 19:10:13 -0700

    Changed data stream, fixed fetch bug

--- app/src/components/Homepage.vue
+++ app/src/components/Homepage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		<WorldTable/>
-		<CountriesTable v-bind:countryData = "countryData"/>
+		<WorldTable v-bind:worldData="worldData"/>
+		<CountriesTable v-bind:countryData="countryData"/>
 	</div>
 </template>
 
@@ -15,30 +15,7 @@ export default {
 		WorldTable,
 		CountriesTable
 	},
-	data(){
-		return {
-			countryData: this.fetchCountryData(),
-		}
-	},
-	methods :  {
-		fetchCountryData(){
-			fetch('https://db.covidbase.app/api/data')
-				.then(response => { 
-					if(response.ok){
-						return response.json()
-					} else {
-					alert("Server returned " + response.status + " : " + response.statusText);
-					}                
-				})
-				.then(response => {
-					sessionStorage.setItem('sortedBy', 'totalD');
-					this.countryData = response;
-				})
-				.catch(err => {
-					console.log(err);
-				});
-		}
-	}
+	props: ["worldData", "countryData"]
 }
 </script>
 
