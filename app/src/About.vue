97046be07c9637fd6b1246ef2e522e6326bd64d3
Josh Hunter <j6hunter@ucsd.edu>
2020-06-11 19:35:16 -0700

    Added temp message

--- app/src/About.vue
+++ app/src/About.vue
@@ -1,5 +1,12 @@
 <template>
-	<div>
-		Scources include CDC, and 
+	<div >
+		<br>
+		<br>
+		<h2><p>I'm working on an about page as you read this.</p>
+		</h2>
 	</div>
 </template>
+
+<style scoped>
+
+</style>
