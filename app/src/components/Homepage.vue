a79307a9ee1809c14e489f3b351721b2c73e620b
Josh Hunter <j6hunter@ucsd.edu>
2020-06-23 01:42:51 -0700

    preparing for continent data, nothing there yet

--- app/src/components/Homepage.vue
+++ app/src/components/Homepage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		<DataTable id="WorldTable" v-bind:data="worldData" v-bind:title="'Global Info'"/>
-		<CountriesTable/>
+		<DataTable id="WorldTable" v-bind:data="worldData" v-bind:moreData="continentData" v-bind:title="'Global Info'"/>
+		<CountriesTable id="countriesTable"/>
 	</div>
 </template>
 
@@ -17,7 +17,10 @@ export default {
 	},
 	computed: {
 		worldData() {
-			return this.$store.state.worldData
+			return this.$store.state.world.worldData
+		},
+		continentData() {
+			return this.$store.state.world.continentData
 		}
 	}
 }
@@ -26,4 +29,37 @@ export default {
 
 
 <style>
+/*smaller screens*/
+@media only screen and (max-width: 1100px){
+	#WorldTable .expend2{display: none;}
+	#WorldTable .rowElement{width: calc(100%/6);}
+}
+
+@media only screen and (max-width: 811px){
+	#WorldTable .expend1{display: none;}
+	#WorldTable .rowElement{width: calc(100%/5);}
+}
+@media only screen and (max-width: 550px){
+	#WorldTable .worldRow {font-size: 85%;}
+	#WorldTable .title{font-size: 125%;}
+}
+@media only screen and (max-width: 465px){
+	#WorldTable .worldRow {font-size: 70%;}
+	#WorldTable .title{font-size: 110%;}
+}
+@media only screen and (max-width: 400px){
+	#WorldTable .worldRow {font-size: 65%;}
+	#WorldTable .title{font-size: 105%;}
+}
+@media only screen and (max-width: 360px){
+	#WorldTable .worldRow {font-size: 62%;}
+	#WorldTable .title{font-size: 102%;}
+}
+@media only screen and (max-width: 340px){
+	#WorldTable .worldRow {font-size: 58%;}
+	#WorldTable .title{font-size: 98%;}
+}
+
+
+
 </style>
