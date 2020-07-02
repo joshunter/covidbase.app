0ffd4f3916b08be4a89a9ae6a2c49f400d57ddd8
Josh Hunter <j6hunter@ucsd.edu>
2020-07-02 14:04:36 -0700

    changed about messsage

--- app/src/About.vue
+++ app/src/About.vue
@@ -1,46 +1,58 @@
 <template>
-	<div >
-		<br>
-		<br>
-		<h2><p>I'm working on an about page as you read this.</p></h2>
-		<br>
-		In the meantime,<br>
-		help me keep this site ad-free: <a href="https://ko-fi.com/joshunter" class="covfefeIcon"><img src="./components/images/kofi(small).png" alt="HTML5 Icon" style="width:27.6px;height:18px;"></a>
+	<div class="paddy">
+		<h3>Thanks for checking out my website!</h3><br>
+		<span>
+			<p>
+				I wanted to create something that was intuative enough and provided infomation quickly and functionally
+				without bombarding the user with a bulky website.
+			</p>
+			<p>
+				COVID-19 is a global issue after all, and not everyone has the resources to regularly download entire applications, 
+				but everyone has the right to stay informed. 
+			</p>
+			<p>
+				I hope you're able to use the website as I intended it.
+			</p>
+		</span>
 
-		<br>
-		email me with comments/suggestions: <a href="mailto:josh@covidbase.app" class="email">josh@covidbase.app</a>
 		<div class="sources">
 			<h2><p>Sources:</p></h2>
 			<a href="https://cloud.google.com/blog/products/data-analytics/free-public-datasets-for-covid19"><img class="sourceImage" src="./components/images/google_cloud_logo.png" alt="gcloud_logo"></a>
-
 			<a href="https://www.worldometers.info/coronavirus/"><img class="sourceImage" src="./components/images/worldometers-logo.jpg" alt="WoM_Logo"></a>
 		</div>
-		
-		
+
+		<p>
+			help me keep the lights on: <a href="https://ko-fi.com/joshunter" class="covfefeIcon"><img class="covfefe" src="./components/images/kofi(small).png" alt="HTML5 Icon" style="width:27.6px;height:18px;"></a><br>
+			comments & suggestions: <a href="mailto:josh@covidbase.app" class="email">josh@covidbase.app</a>
+		</p>
 	</div>
 </template>
 
 <style scoped>
+p{padding: 7px;}
+
+.paddy{padding: 40px;}
+
 .email {
 	text-decoration: none;
 	color: #d8dbe2;
 }
-.email:hover{
-	text-decoration: underline;
-}
-.lightMode .email{
-	color:#3c3c3c;
-}
-.covfefeIcon {
-	padding-bottom: 3px;
-}
+.email:hover{text-decoration: underline;}
+.lightMode .email{color:#3c3c3c;}
+
+.covfefeIcon {padding-bottom: 3px;}
+
+.covfefe{transition:1s;}
+.covfefe:hover {transform: rotate(360deg);}
+
 .sourceImage{
-	margin: 20px;
-	width: 195px;
+	margin: 20px
+;	width: 195px;
 	height: 52px;
 	border-radius: 7px;
 }
 .sources {
-	padding: 20px;
+	padding-top: 80px;
+	padding-bottom: 70px;
 }
 </style>
